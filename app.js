bdce132b7909d55afce4d62062ed36ffed94e6c0
thanhnd <thanhnd@bap.jp>
2022-02-21 20:54:44 +0700

    update source octivan

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+  const menuWrap = document.querySelector(".header-menu-wrap");
+  const openMenu = document.querySelector(".header-open-menu");
+  const closeMenu = document.querySelector(".header-close-menu");
+
+  openMenu.addEventListener("click", function () {
+    menuWrap.classList.add("header-menu-wrap-active");
+    return false;
+  });
+
+  closeMenu.addEventListener("click", function () {
+    menuWrap.classList.remove("header-menu-wrap-active");
+  });
+
+  document.addEventListener("click", function (e) {
+    console.log("menuWrap", menuWrap.contains(e.target));
+    if (
+      !menuWrap.contains(e.target) &&
+      !e.target.matches(".header-open-menu")
+    ) {
+      menuWrap.classList.remove("header-menu-wrap-active");
+    }
+  });
+});
